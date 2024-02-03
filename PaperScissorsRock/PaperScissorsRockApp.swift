af8c670a0c43a625302da21a8028f5af33b93b03
Baymurat785 <abdimuratovbaymurat03@gmail.com>
2024-02-03 15:06:14 +0500

    Initial Commit

---
+++ PaperScissorsRock/PaperScissorsRockApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaperScissorsRockApp.swift
+//  PaperScissorsRock
+//
+//  Created by Baymurat Abdumuratov on 03/02/24.
+//
+
+import SwiftUI
+
+@main
+struct PaperScissorsRockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
